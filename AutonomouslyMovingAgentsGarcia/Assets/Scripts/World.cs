16a509414d08df14b40a0f0caf780173a9d7396d
Xx-Diego-XX-Agustin-xx-Garcia-xX <720791@student.dusd.net>
2024-03-21 12:35:30 -0700

    Completed
    q

---
+++ AutonomouslyMovingAgentsGarcia/Assets/Scripts/World.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class World {
+
+    static readonly World instance = new World();
+    static GameObject[] hidingSpots;
+
+    static World() {
+
+        hidingSpots = GameObject.FindGameObjectsWithTag("Hide");
+    }
+
+    World() { }
+    public static World Instance { get { return instance; } }
+    public GameObject[] GetHidingSpots() { return hidingSpots; }
+}
